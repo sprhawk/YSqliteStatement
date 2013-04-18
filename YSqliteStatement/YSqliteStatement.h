7fd787966f40803515af49b2998c4c2e19ac91ce
sprhawk <hongbo@yang.me>
2013-04-18 15:05:29 +0800

    Initial Commit

---
+++ YSqliteStatement/YSqliteStatement.h
@@ -0,0 +1,13 @@
+//
+//  YSqliteStatement.h
+//  YSqliteStatement
+//
+//  Created by YANG HONGBO on 2013-4-18.
+//  Copyright (c) 2013年 YANG HONGBO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YSqliteStatement : NSObject
+
+@end
---
+++ YSqliteStatement/YSqliteStatement.m
@@ -0,0 +1,13 @@
+//
+//  YSqliteStatement.m
+//  YSqliteStatement
+//
+//  Created by YANG HONGBO on 2013-4-18.
+//  Copyright (c) 2013年 YANG HONGBO. All rights reserved.
+//
+
+#import "YSqliteStatement.h"
+
+@implementation YSqliteStatement
+
+@end
